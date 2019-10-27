166fe23a5d71dc846152681a0b5632afec561715
BigWhiteCrayon <jordanryoungberg@gmail.com>
2019-10-26 22:33:18 -0500

    Added File Upload Logic

--- routes/location.js
+++ routes/location.js
@@ -1,3 +1,5 @@
+require('dotenv').config
+
 const express = require('express');
 const router = express.Router();
 const Object = require('../models/Object');
@@ -5,6 +7,23 @@ const mongoose = require('mongoose');
 
 router.get('/', (req, res) => {
     const {body} = req.body;
+})
+
+router.post('/model', async(req, res) =>{
+    try{
+        if(!req.files){return res.status(500).send({message: 'No File Uploaded'})};
+
+        let model = req.files.model;
+
+        model.mv('./public/' + model.name, (err) => {
+            return res.status(500).send(err);
+        });
+        let path = 'http://localhost:' + process.env.PORT + '/' + model.name;
+        res.status(200).send({ path: path});
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 })
 
 module.exports = router;
